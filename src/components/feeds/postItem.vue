b172c51ef6820cd6477ac4a0d05cc054d710d423
ilyalopatin1994 <shark132@yandex.ru>
2022-03-28 01:23:07 +0300

    Изменено выравнивание текста в кнопках

--- src/components/feeds/postItem.vue
+++ src/components/feeds/postItem.vue
@@ -9,12 +9,12 @@
       <span class="">{{ post.postInfo.description }}</span>
       <div class="actions">
         <div class="btn" id="btnStar" @click="increment($event)">
-          <img src="star.png" />
+          <img class="icon" src="star.png" />
           Star
         </div>
         <div class="btn" id="btnTextStar">{{ post.postInfo.stars }}</div>
         <div class="btn" id="btnFork" @click="increment($event)">
-          <img src="fork.png" />
+          <img class="icon" src="fork.png" />
           Fork
         </div>
         <div class="btn" id="btnTextFork">
@@ -81,6 +81,9 @@ export default {
   margin-right: 14px;
 }
 
+.icon {
+  margin-right: 4.5px;
+}
 .post {
   width: 100%;
   height: 173px;
@@ -101,24 +104,26 @@ export default {
 .btn {
   height: 28px;
   box-sizing: border-box;
+  display: flex;
+  align-items: center;
 }
 
 #btnStar {
   border-radius: 6px 0px 0px 6px;
   border: 1px solid rgba(27, 31, 35, 0.15);
-  padding: 3px 13px 5px 12px;
+  padding: 0px 13px 0px 12px;
 }
 
 #btnTextStar {
   border: 1px solid rgba(27, 31, 35, 0.15);
   border-left: 0px;
-  padding: 5px 12px 5px 11px;
+  padding: 0px 12px 0px 11px;
 }
 
 #btnFork {
   border: 1px solid rgba(27, 31, 35, 0.15);
   border-left: 0px;
-  padding: 3px 13px 5px 12px;
+  padding: 0px 13px 0px 12px;
 }
 
 #btnTextFork {
