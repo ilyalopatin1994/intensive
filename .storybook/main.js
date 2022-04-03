46e28954336b16771992bb29bc76765cd167a753
ilyalopatin1994 <shark132@yandex.ru>
2022-04-04 01:25:31 +0300

    Папка storybook

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs"
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "webpack5"
+  }
+}
